9903e4f1e6d9ffdab81c03748365f90a307f5eca
babylee2002 <babylee2002@gmail.com>
2018-02-24 16:36:50 -0500

    working on Azure API

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -1,39 +1,20 @@
 class Api::LessonsController < ApplicationController
   def index
     @lessons = Language.find(params[:language_id]).lessons
-     # include HTTParty
-     require 'net/https'
-     require 'uri'
-     require 'cgi'
-     require 'json'
-   
-   # **********************************************
-   # *** Update or verify the following values. ***
-   # **********************************************
-   
-   # Replace the key string value with your valid subscription key.
-   key = '6a54283bc5134e1f8058ec86bd9fa516'
-   
-   host = 'https://api.microsofttranslator.com'
-   path = '/V2/Http.svc/Translate'
-   
-   target = 'ko'
-   text = 'I am a girl'
-   
-   params = '?to=' + target + '&text=' + CGI.escape(text)
-   uri = URI (host + path + params)
-   
-   request = Net::HTTP::Get.new(uri)
-   request['Ocp-Apim-Subscription-Key'] = key
-   
-   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-       http.request (request)
-   end
-   
- 
- 
-   puts @api_response = Hash.from_xml(response.body).to_json
- puts response.body
+
     render json: @lessons 
+  end
+
+  def create
+
+  @lessons = Lesson.create!( lesson_params )
+
+    render json: @lessons
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :audio, :question, :answer,:user_id, :language_id)
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,42 @@
 class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :language
+  
+  # include HTTParty
+     require 'net/https'
+     require 'uri'
+     require 'cgi'
+     require 'json'
+     require 'nokogiri'
+   
+   # **********************************************
+   # *** Update or verify the following values. ***
+   # **********************************************
+   
+   # Replace the key string value with your valid subscription key.
+  #  def self.generate(api_id)
+   key = '6a54283bc5134e1f8058ec86bd9fa516'
+   host = 'https://api.microsofttranslator.com'
+   path = '/V2/Http.svc/Translate'
+   
+   target = 'ko'
+   text = 'I am a girl'
+   
+   params = '?to=' + target + '&text=' + CGI.escape(text)
+   uri = URI (host + path + params)
+   
+   request = Net::HTTP::Get.new(uri)
+   request['Ocp-Apim-Subscription-Key'] = key
+   
+   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+       http.request (request)
+   end
+   
+  puts data = Hash.from_xml(response.body)
+  puts api_res = data["string"]
+   
+
+  @lessons = Lesson.create!( {title:text, audio:nil, question:text, answer:api_res,user_id:1, language_id:1} )
   end
+# end
 
