e0cd11ea80154083717f1181b0fd5ad0eb1b6584
babylee2002 <babylee2002@gmail.com>
2018-02-26 11:15:42 -0500

    axios call made in LessonBox

--- client/src/Components/LessonBox.js
+++ client/src/Components/LessonBox.js
@@ -3,18 +3,30 @@ import axios from 'axios'
 
 class LessonBox extends Component {
 
-//Using axios to get all the users
-async componentWillMount() {
-    const res = await axios.get('/api/languages')
-    console.log('USERS:' + res.data)
-    this.setState({ users: res.data })
-}
+    state = {
+        languages: [],
+        newlanguage: {},
+        redirect: false
+    }
+
+    //Using axios to get all the users
+    async componentWillMount() {
+        const res = await axios.get('/api/languages')
+        console.log('Languages:' + JSON.stringify(res.data))
+        this.setState({ languages: res.data })
+    }
 
 
     render() {
         return (
             <div>
-                
+                {this.state.languages.map((language, i) => {
+                    return (
+                        <div key={1}>
+                        language:{language.name}
+                        </div>
+                    )
+                })}
             </div>
         );
     }
