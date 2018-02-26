7244061ecc87fd0269db763425d4320dac291d12
babylee2002 <babylee2002@gmail.com>
2018-02-26 11:49:13 -0500

    seeing again

--- client/src/Components/LanguageList.js
+++ client/src/Components/LanguageList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Select from 'react-select'
+
 import 'react-select/dist/react-select.css';
 
 import LessonBox from './LessonBox.js'
@@ -7,19 +9,41 @@ import LessonBox from './LessonBox.js'
 class LanguageList extends Component {
     state = {
         selectedOption: '',
+        languages: [],
+        newlanguage: {},
+        redirect: false
     }
+   
+
+    //Using axios to get all the users
+    async componentWillMount() {
+        const res = await axios.get('/api/languages')
+        console.log('Languages:' + JSON.stringify(res.data))
+        this.setState({ languages: res.data })
+    }
+
     handleChange = (selectedOption) => {
         this.setState({ selectedOption });
         console.log(`Selected: ${selectedOption.label}`);
     }
     render() {
         const { selectedOption } = this.state;
+        const {languages} = this.state;
         const value = selectedOption && selectedOption.value;
         const style = {
-            width: '50vh',
+            width: '50vh'
         }
         return (
             <div>
+                {languages.map((language, i) => {
+                    return (
+                        <div key={1}>
+                        language:{language.name}<br/>
+                        location:{language.location}<br/>
+                        language:<img src={language.img_url}/><br/>
+                        </div>
+                    )
+                })}
             <Select
                 name="form-field-name"
                 value={value}
--- client/src/Components/LessonBox.js
+++ client/src/Components/LessonBox.js
@@ -3,30 +3,13 @@ import axios from 'axios'
 
 class LessonBox extends Component {
 
-    state = {
-        languages: [],
-        newlanguage: {},
-        redirect: false
-    }
-
-    //Using axios to get all the users
-    async componentWillMount() {
-        const res = await axios.get('/api/languages')
-        console.log('Languages:' + JSON.stringify(res.data))
-        this.setState({ languages: res.data })
-    }
+  
 
 
     render() {
         return (
             <div>
-                {this.state.languages.map((language, i) => {
-                    return (
-                        <div key={1}>
-                        language:{language.name}
-                        </div>
-                    )
-                })}
+                
             </div>
         );
     }
