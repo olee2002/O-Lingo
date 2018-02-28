171be215caf1dc22f58695b81bba607df2d232bc
babylee2002 <babylee2002@gmail.com>
2018-02-28 18:29:44 -0500

    testing for heroku

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'olees.auth0.com',
   clientId: 'SnVcPvOB5bL9aycX5XV9t1iadr2wCP5F',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://o-lingo.herokuapp.com/callback'
 }
