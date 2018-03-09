eca61851ef5ec2fe91411db9515b69914dd307ef
babylee2002 <babylee2002@gmail.com>
2018-03-09 10:55:01 -0500

    Auth0-variable changed

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'olees.auth0.com',
   clientId: 'SnVcPvOB5bL9aycX5XV9t1iadr2wCP5F',
-  // callbackUrl: 'http://localhost:3000/callback',
-  callbackUrl: 'https://o-lingo.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/callback',
+  // callbackUrl: 'https://o-lingo.herokuapp.com/callback'
 }
